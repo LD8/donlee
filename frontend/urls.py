94e09a317b8089b1a3b27cf73fbb43b83366cb92
ld8 <don_lee@me.com>
2020-04-30 12:31:09 +0800

    negated (?!admin) from frontend url path

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     # path() method doesn't work with react router for some reason
     # `(?!api)` negative lookahead, not a match if the path starts with 'api'
-    re_path(r'^(?!api)(?:.*)/?$', views.index),
+    re_path(r'^(?!api)(?!admin)(?:.*)/?$', views.index),
 ]
