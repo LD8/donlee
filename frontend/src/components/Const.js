24602da73731989c6325628e8b2f273af754f227
ld8 <don_lee@me.com>
2020-04-30 11:44:58 +0800

    changed APIBASE url to donlee.online

--- frontend/src/components/Const.js
+++ frontend/src/components/Const.js
@@ -1,4 +1,5 @@
-export const APIBASE = "http://127.0.0.1:8000/api";
+export const APIBASE = "http://donlee.online/api";
+// export const APIBASE = "http://127.0.0.1:8000/api";
 
 // posts mockup data
 export const posts = [
