36aec871817aa3792adb572646b4ea216b785e72
ld8 <don_lee@me.com>
2020-04-30 13:03:10 +0800

    typo: forward slash before frontend deleted

--- donlee/settings.py
+++ donlee/settings.py
@@ -120,13 +120,13 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# 12 files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     # This lets Django's collectstatic store our bundles
-    os.path.join(BASE_DIR, '/frontend/src/assets'),
+    os.path.join(BASE_DIR, 'frontend/src/assets'),
 )
 
 LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
